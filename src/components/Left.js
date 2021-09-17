5b91434ac91a254a4d9786dcfcdf104ff5961e61
NoboruWataya123 <jinxw666@gmail.com>
2021-09-17 17:26:52 +0900

    fix: remove not interactive part

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,65 +1,40 @@
-import React, {useEffect, useState} from 'react'
-import styles from './Left.module.css'
-import { fetchData } from '../utils/fetch'
-import {getDayOfWeek} from './Right'
+import React, { useEffect, useState } from "react";
+import styles from "./Left.module.css";
+import { fetchData } from "../utils/fetch";
+import { getDayOfWeek } from "./Right";
 
 export default function Left() {
-    const [temperature, setTemperature] = useState(0)
-    const [description, setDescription] = useState('')
-    const [symbol, setSymbol] = useState('')
+  const [temperature, setTemperature] = useState(0);
+  const [description, setDescription] = useState("");
+  const [symbol, setSymbol] = useState("");
 
-    useEffect(() => {
-        fetchData(`current/102013159&lang=ru`).then(data => {
-            setTemperature(data.current.temperature)
-            setDescription(data.current.symbolPhrase)
-            setSymbol(data.current.symbol)
-        })
-    }, [])
+  useEffect(() => {
+    fetchData(`current/102013159&lang=ru`).then((data) => {
+      setTemperature(data.current.temperature);
+      setDescription(data.current.symbolPhrase);
+      setSymbol(data.current.symbol);
+    });
+  }, []);
 
-    // useEffect(() => {
-    //     fetchData().then(data => {
-    //         setTemperature(data.forecast[0].maxTemp)
-    //         setDescription(data.forecast[0].symbolPhrase)
-    //         setSymbol(data.forecast[0].symbol)
-    //         setDate(data.forecast[0].date)
-    //     })
-    // }, [])
-    // const url = process.env.REACT_APP_BASE_URL
-    // const token = process.env.REACT_APP_TOKEN
-    // // fetch data from API with token
-    // useEffect(() => {
-    //     fetch(`${url}&token=${token}` ,{
-    //         method: 'GET',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     })
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         console.log(data)
-    //         setTemperature(data.forecast[0].maxTemp)
-    //         setDescription(data.forecast[0].symbolPhrase)
-    //         setSymbol(data.forecast[0].symbol)
-    //     })
-    // }, [url, token])
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.left}>
-                <button className={styles.search}>
-                Searh for place
-                </button>
-            </div>
-            <div className={styles.image}>
-                <img src={`https://developer.foreca.com/static/images/symbols/${symbol}.png`} alt="symbol" className={styles.symbol}/>
-            </div>
-            <div className={styles.temperature}>
-                <p>{temperature}℃</p>
-            </div>
-            <div className={styles.description}>
-                <p>{description}</p>
-                <p className={styles.date}>{getDayOfWeek(Date.now())}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <button className={styles.search}>Searh for place</button>
+      </div>
+      <div className={styles.image}>
+        <img
+          src={`https://developer.foreca.com/static/images/symbols/${symbol}.png`}
+          alt="symbol"
+          className={styles.symbol}
+        />
+      </div>
+      <div className={styles.temperature}>
+        <p>{temperature}℃</p>
+      </div>
+      <div className={styles.description}>
+        <p>{description}</p>
+        <p className={styles.date}>{getDayOfWeek(Date.now())}</p>
+      </div>
+    </div>
+  );
 }
--- src/components/Right.js
+++ src/components/Right.js
@@ -1,68 +1,64 @@
-import React, {useState, useEffect} from 'react'
-import styles from './Right.module.css'
-import { fetchData } from '../utils/fetch'
-
+import React, { useState, useEffect } from "react";
+import styles from "./Right.module.css";
+import { fetchData } from "../utils/fetch";
 
 // const url = process.env.REACT_APP_BASE_URL
 // const token = process.env.REACT_APP_TOKEN
 
 export function getDayOfWeek(date) {
-    const dayOfWeek = new Date(date).getDay();    
-    return isNaN(dayOfWeek) ? null : 
-      ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'][dayOfWeek];
-  }
+  const dayOfWeek = new Date(date).getDay();
+  return isNaN(dayOfWeek)
+    ? null
+    : [
+        "Воскресенье",
+        "Понедельник",
+        "Вторник",
+        "Среда",
+        "Четверг",
+        "Пятница",
+        "Суббота",
+      ][dayOfWeek];
+}
 
 export default function Right() {
-    const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        fetchData(`forecast/daily/102013159&lang=ru&dataset=full`).then(data => {
-            setData(data.forecast)
-        })
-    }, [])
+  useEffect(() => {
+    fetchData(`forecast/daily/102013159&lang=ru&dataset=full`).then((data) => {
+      setData(data.forecast);
+    });
+  }, []);
 
-    
-    
-    return (
-        <div className={styles.container}>
-            <div className={styles.cards}>
-                {data && data.slice(1).map(item => 
-                    <div key={item.date} className={styles.card}>
-                        <div className={styles.card_title}>
-                            <h3>{getDayOfWeek( item.date )} <br /> {item.date}</h3>
-                        </div>
-                        <div className={styles.img}>
-                            <img src={`https://developer.foreca.com/static/images/symbols/${item.symbol}.png`} alt="symbol" className={styles.symbol}/>
-                        </div>
-                        <div className={styles.card_content}>
-                            <div className={styles.card_content_item}>
-                                <p className={styles.header_three}>{item.maxTemp}°C</p>
-                                <p className={styles.header_three}>{item.minTemp}°C</p>
-                            </div>
-                            <div className={styles.card_desc}>
-                                <p>{item.symbolPhrase}</p>
-                            </div>
-                        </div>
-                    </div>
-                )}
-            </div>
-            <div className={styles.block_desc}>
-                    <div className={styles.block_card}>
-                        <p>Wind status</p>
-                        <p>7 mph</p>
-                    </div>
-                    <div className={styles.block_card}>
-                        <p>Humidity</p>
-                        <p>84%</p>
-                        <div className={styles.line}>
-                            <div className={styles.bar}></div>
-                        </div>
-                    </div>
-                    <div className={styles.block_card}>
-                        <p>Visibility</p>
-                        <p>7 miles</p>
-                    </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.cards}>
+        {data &&
+          data.map((item) => (
+            <div key={item.date} className={styles.card}>
+              <div className={styles.card_title}>
+                <h3>
+                  {getDayOfWeek(item.date)} <br /> {item.date}
+                </h3>
+              </div>
+              <div className={styles.img}>
+                <img
+                  src={`https://developer.foreca.com/static/images/symbols/${item.symbol}.png`}
+                  alt="symbol"
+                  className={styles.symbol}
+                />
+              </div>
+              <div className={styles.card_content}>
+                <div className={styles.card_content_item}>
+                  <p className={styles.header_three}>{item.maxTemp}°C</p>
+                  <p className={styles.header_three}>{item.minTemp}°C</p>
                 </div>
-        </div>
-    )
+                <div className={styles.card_desc}>
+                  <p>{item.symbolPhrase}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
